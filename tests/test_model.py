7626f3fdd66c6c469eee9db7d068fdb546aec532
Juan Trebolle <juan.itrebolle@gmail.com>
2025-01-16 16:58:34 +0100

    Updated test_model.py - works with new model :)

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,34 +1,37 @@
 import torch
 import pytest
 
-#from my_project.models import MyModel
-#from mlsopsbasic import MyAwesomeModel
-from mlsopsbasic.models.unet import UNet
-from mlsopsbasic.config import NUM_CLASSES
+from mlsopsbasic.models.model import SegmentationModel
+
+NUM_CLASSES = 11
 
 @pytest.fixture
 def model():
     """Fixture to initialize and return the model."""
-    # return MyAwesomeModel()
-    #return UNet(channels=[3, 64, 128, 256, 1024], out_channels=11)
-    return UNet(channels=[3, 64, 128, 256, 512], out_channels=NUM_CLASSES)
+    return SegmentationModel(pretrained=True, num_classes=NUM_CLASSES)
+
+def test_model_initialization():
+    """Test that the model initializes with different configurations."""
+    model1 = SegmentationModel(pretrained=True, num_classes=NUM_CLASSES)
+    assert isinstance(model1, SegmentationModel)
+
+    model2 = SegmentationModel(pretrained=False, num_classes=NUM_CLASSES)
+    assert isinstance(model2, SegmentationModel)
+
 
 def test_model_output_shape(model):
-    """Test that the model produces and output of the expected shape given an input - COCO-style input"""
-    input_shape = (1, 3, 512, 512) # (batch_size, channels, height, width)
+    batch_size = 4
+    channels = 3
+    height, width = 512, 512
+    input_shape = (batch_size, channels, height, width)
+    expected_output_shape = (batch_size, NUM_CLASSES, height, width)
 
-    #expected_output_shape = (1, 1000) # (batch_size, number of classes)
-    expected_output_shape = torch.Size((1, NUM_CLASSES, 512, 512)) # (batch_size, num_classes, height, width)
+    dummy_input = torch.randn(*input_shape)
+    model_output = model(dummy_input)
 
-    dummy_input = torch.randn(*input_shape)  # Create a dummy input tensor
-    model_output = model(dummy_input) # Get the model's output
+    assert isinstance(model_output, dict), "Model output should be a dictionary"
+    assert "out" in model_output, "Model output dictionary should contain 'out' key"
 
-    # Compare shapes, not the tensor itself
-    assert model_output.shape == expected_output_shape, (
-            f"Expected output shape {expected_output_shape}, but got {model_output.shape}"
-    )
+    result_shape = model_output['out'].shape
 
-    # Additional check to verify number of classes
-    assert model_output.shape[1] == NUM_CLASSES, (
-        f"Expected {NUM_CLASSES} output channels but got {model_output.shape[1]}"
-    )
+    assert result_shape == expected_output_shape, f"Expected output shape {expected_output_shape}, but got {result_shape}"
